b1bab241081d37ea13893ab29b33cbbd4bb64eeb
con-j-e <conjedick@gmail.com>
2025-09-11 11:57:20 -0800

    initial admin env directory

---
+++ admin/scripts/env/setup.ps1
@@ -0,0 +1,35 @@
+$WhichConda = Get-Command conda.exe
+$CondaPath = $WhichConda.Path
+$CondaVersion = & $CondaPath --version
+
+$Response = Read-Host ("${CondaVersion} found at ${CondaPath}. Would you like to use this conda installation to conduct the environment setup? ( y / n )")
+if (-not ($Response -match '^[Yy]$')) {
+    Write-Host "Aborted by user."
+    exit
+}
+$Response = Read-Host ("This setup script is executing from the directory ${PSScriptRoot}. Would you like to setup the environment in this location? ( y / n )")
+if (-not ($Response -match '^[Yy]$')) {
+    Write-Host "Aborted by user."
+    exit
+}
+
+Set-Location $PSScriptRoot
+Write-Host "Beginning setup..."
+& conda env create --prefix .\conda_env --file .\environment.yml
+& conda activate .\conda_env
+if ($env:CONDA_DEFAULT_ENV) {
+    $Response = Read-Host ("Conda environment '$($env:CONDA_DEFAULT_ENV)' is now active. Would you like the setup script to proceed with the activated environment? ( y / n )")
+} else {
+    Write-Host "Unable to activate conda environment. Shell profile may require 'conda init' before this setup script will work as expected. Exiting..."
+    exit
+}
+if (-not ($Response -match '^[Yy]$')) {
+    Write-Host "Aborted by user."
+    exit
+}
+& conda install -c esri geomet=1.0.0
+& pip install -e (Join-Path $Env:AKDOF_ROOT "\library\akdof_shared")
+
+Write-Host "Conda environment '$($env:CONDA_DEFAULT_ENV)' setup complete. Exiting..."
+& conda deactivate
+exit
